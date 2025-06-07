1ab4e489b8cd7b5d99c234d6f0872bdd6e227c48
nullcyberx <116954051+nullcyberx@users.noreply.github.com>
2025-06-07 12:32:14 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1749279734061

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_ae1a2735._.js",
+  "static/chunks/src_29b3589a._.js"
+],
+    source: "dynamic"
+});
